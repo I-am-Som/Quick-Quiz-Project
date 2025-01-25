405f5a9351f9f8e57b7ff704300f2a67c48f10b3
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-01-25 21:55:22 +0530

    ---

--- quizAppFrontend/src/components/Quiz.jsx
+++ quizAppFrontend/src/components/Quiz.jsx
@@ -2,7 +2,7 @@ function QuizPage() {
     return (
         <div className="h-screen w-screen bg-amber-200 flex justify-center items-center">
             <div className="h-[70%] w-[60%] rounded-lg bg-amber-300">
-
+                
             </div>
         </div>
     );
