c7c3302032dc599db1652841fda64c0a9f7ff21c
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-01-27 21:42:40 +0530

    changes

--- quizAppFrontend/src/components/Quiz.jsx
+++ quizAppFrontend/src/components/Quiz.jsx
@@ -1,8 +1,19 @@
 function QuizPage() {
     return (
-        <div className="h-screen w-screen bg-amber-200 flex justify-center items-center">
-            <div className="h-[70%] w-[60%] rounded-lg bg-amber-300">
-                
+        <div className="h-screen w-screen bg-white flex justify-center items-center">
+            <div className="h-[80%] w-[65%] rounded-lg bg-rose-50 flex-row space-y-3">
+                <button className="h-[9%] w-[80%] bg-amber-100 rounded-md">
+                    option 1
+                </button>
+                <button className="h-[9%] w-[80%] bg-amber-100 rounded-md">
+                    option 2
+                </button>
+                <button className="h-[9%] w-[80%] bg-amber-100 rounded-md">
+                    option 3
+                </button>
+                <button className="h-[9%] w-[80%] bg-amber-100 rounded-md">
+                    option 4
+                </button>
             </div>
         </div>
     );
