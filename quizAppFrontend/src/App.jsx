ebe1804fcbf229bc8f611013212a390642b8bc5b
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-01-29 20:46:09 +0530

    ..

--- quizAppFrontend/src/App.jsx
+++ quizAppFrontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
 import Quiz from './components/Quiz'; // Fixed import path
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="h-screen w-screen">
+      <Login />
       <Quiz />
     </div>
   );
--- quizAppFrontend/src/components/Quiz.jsx
+++ quizAppFrontend/src/components/Quiz.jsx
@@ -1,19 +1,24 @@
 function QuizPage() {
     return (
         <div className="h-screen w-screen bg-white flex justify-center items-center">
-            <div className="h-[80%] w-[65%] rounded-lg bg-rose-50 flex-row space-y-3">
-                <button className="h-[9%] w-[80%] bg-amber-100 rounded-md">
-                    option 1
-                </button>
-                <button className="h-[9%] w-[80%] bg-amber-100 rounded-md">
-                    option 2
-                </button>
-                <button className="h-[9%] w-[80%] bg-amber-100 rounded-md">
-                    option 3
-                </button>
-                <button className="h-[9%] w-[80%] bg-amber-100 rounded-md">
-                    option 4
-                </button>
+            <div className="h-[80%] w-[65%] rounded-lg bg-gray-100 border-slate-300 border-[1px] flex flex-col justify-center items-center space-y-3">
+                <h6 className="font-semibold">Question{}</h6>
+                
+                    <div className="w-full h-[10%] self-start mt-3 flex flex-col items-center jsutify-center">
+                        <p className="font-sans text-md">who are you boy?</p>
+                    </div>
+                    <button className="h-[9%] w-[80%] bg-gray-200 border-slate-300 border-[1px] hover:bg-gray-300 rounded-md">
+                        option 1
+                    </button>
+                    <button className="h-[9%] w-[80%] bg-gray-200 border-slate-300 border-[1px] hover:bg-gray-300 rounded-md">
+                        option 2
+                    </button>
+                    <button className="h-[9%] w-[80%] bg-gray-200 border-slate-300 border-[1px] hover:bg-gray-300 rounded-md">
+                        option 3
+                    </button>
+                    <button className="h-[9%] w-[80%] bg-gray-200 border-slate-300 border-[1px] hover:bg-gray-300 rounded-md">
+                        option 4
+                    </button>
             </div>
         </div>
     );
